5c0cdf5503e64aaa102621db0b04ce1649bbd66d
Clover Ison <clover@isons.org>
2026-08-06 11:47:43 -0700

    maid: Move mako config out of HM

---
+++ domus/mako/default.nix
@@ -0,0 +1,45 @@
+{ config, lib, pkgs, inputs, ... }: {
+
+options = {
+  desktop.mako.enable = lib.mkEnableOption "mako notification daemon";
+};
+
+config = lib.mkIf config.desktop.mako.enable {
+  users.users.cison.maid = {
+    packages = [ pkgs.mako ];
+    systemd.packages = [ pkgs.mako ];
+
+    file.xdg_data."dbus-1/services/fr.emersion.mako.service" = {
+      source = pkgs.mako ++ "/share/dbus-1/servies/fr.emersion.mako.service";
+    };
+    # TODO: this should be a wrapper (with a test)
+    file.xdg_config."mako/config".text = with inputs.pyroscheme.lib.colors; ''
+      anchor=top-right
+      background-color=#${black}
+      border-color=#${blackBright}
+      border-radius=10
+      border-size=1
+      default-timeout=5000
+      font=IBM Plex Mono
+      ignore-timeout=true
+      layer=overlay
+      margin=10
+      markup=true
+      max-visible=10
+      padding=5
+      progress-color=over #${green}
+      text-color=#${white}
+
+      [mode=dnd]
+      invisible=true
+    '';
+  };
+
+  assertions = [
+    { assertion = config.desktop.mako.enable -> config.head.graphical;
+      message = "can't have a notification daemon without a desktop!";
+    }
+  ];
+};
+
+}
