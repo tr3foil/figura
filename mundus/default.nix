c3424f71b3368d32666a4516eebed5909ce03105
Clover Ison <clover@isons.org>
2026-08-12 16:45:19 -0700

    Add yubikey ssh keys and management software

--- mundus/default.nix
+++ mundus/default.nix
@@ -121,6 +121,7 @@ users.users.cison = {
   openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINtQWB9sas10nnFPFaWYMTY+8QrXaJ64bBJz/iGnCtFO cison@sol"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAaOgvgz6SPLK488uiy+U0Vpq2szWzsS6RuvQZ8w9qWP cison@vega"
+    "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIEmwPMMPsx2kNI60yFBGKNnfW6UHsOxLN07IFCjL52iqAAAABHNzaDo= clover@isons.org"
   ];
   initialHashedPassword = "";
 };
@@ -192,6 +193,8 @@ console = {
 };
 
 programs.nano.enable = false;
+
+programs.yubikey-manager.enable = true;
 
 services = {
   smartd = {
